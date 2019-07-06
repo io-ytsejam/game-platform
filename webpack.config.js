ea64f460d759a749d722cfe3a4a93ac96683df4a
io-ytsejam <marcin19981@gmail.com>
2019-07-06 16:38:58 +0200

    Webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ module.exports = {
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "build/"),
     publicPath: "/",
     filename: "bundle.js"
   },
